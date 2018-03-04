3a6e39d5bf19b84532017a7f850c2661ca54b8be
Ian Maurer <ian@genomoncology.com>
2018-03-04 06:27:35 -0500

    Add 5 pages to assets from contractor. Broke layout into smaller includes.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,5 @@
 //flexslider
-
-$('.flexslider').flexslider({ 
+$('#what_clients_say').flexslider({ 
 	controlNav: false, 
 	directionNav: false, 
 	animation: "slide", 
@@ -22,11 +21,65 @@ function closeSearch() {
 
 
 // header bg opacity
+var bannerHeight = $(".section-banner").height() - $(".navbar-fixed-top").height() * 2;
 $(document).on("scroll",function(){
-	if($(document).scrollTop()>200){ 
+	if($(document).scrollTop()>bannerHeight){ 
 		$(".navbar").addClass("small");
-		}
+	}
 	else{
 		$(".navbar").removeClass("small");
-		}
+	}
+});
+
+
+// showing CAPABILITY content
+$( "a.plus-icon-wrap" ).click(function( event ) {
+  event.preventDefault();
+  $(this).hide().parent().next().show();
+});
+
+
+// masornary layout in CAPABILITY section
+$('.grid').masonry({
+  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+ //columnWidth: '.grid-sizer',
+  percentPosition: true,
+});
+
+
+// Resouces page carousel
+$(document).ready(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 273,
+    itemMargin: 50,
+    asNavFor: '#slider',
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel",
+    directionNav:false
+  });
+});
+
+// magnatic popup
+$(document).ready(function() {
+    //youtube video
+    $('.popup-youtube').magnificPopup({
+        //disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false,
+    });
 });
