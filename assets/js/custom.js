4c83086f7f9583a5c1eb91ba69548f011e633a39
Ian Maurer <ian@genomoncology.com>
2018-03-05 08:31:22 -0500

    Final HTML build. Home page setup with flow blocks needed.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,50 +1,46 @@
-//flexslider
-$('#what_clients_say').flexslider({ 
-	controlNav: false, 
-	directionNav: false, 
-	animation: "slide", 
-	slideshowSpeed: 10000, 
-	directionNav: true, 
-	prevText: "Previous",
-	nextText: "Next",
-});
-
- 
 // full screen search form
  function openSearch() {
     document.getElementById("myOverlay").style.display = "block";
 }
-
 function closeSearch() {
     document.getElementById("myOverlay").style.display = "none";
 }
 
 
-// header bg opacity
-var bannerHeight = $(".section-banner").height() - $(".navbar-fixed-top").height() * 2;
+
+// opacity(header bg)
+var bannerHeight = $(".section-banner, .section-hero").height() - $(".navbar-fixed-top").height() * 2;
 $(document).on("scroll",function(){
-	if($(document).scrollTop()>bannerHeight){ 
-		$(".navbar").addClass("small");
-	}
-	else{
-		$(".navbar").removeClass("small");
-	}
+  if($(document).scrollTop()>bannerHeight){
+    $(".navbar").addClass("small");
+  }
+  else{
+    $(".navbar").removeClass("small");
+  }
 });
 
+
+
+
+
+// masornary layout in CAPABILITY section
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+ //columnWidth: '.grid-sizer',
+  percentPosition: true,
+});
 
 // showing CAPABILITY content
 $( "a.plus-icon-wrap" ).click(function( event ) {
   event.preventDefault();
   $(this).hide().parent().next().show();
+  $(this).closest(".box-content").css("margin-top", "0");
+  $grid.masonry("reloadItems");
+  $grid.masonry();
 });
 
 
-// masornary layout in CAPABILITY section
-$('.grid').masonry({
-  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
- //columnWidth: '.grid-sizer',
-  percentPosition: true,
-});
+
 
 
 // Resouces page carousel
@@ -59,7 +55,7 @@ $(document).ready(function() {
     itemMargin: 50,
     asNavFor: '#slider',
   });
- 
+
   $('#slider').flexslider({
     animation: "slide",
     controlNav: false,
@@ -69,6 +65,8 @@ $(document).ready(function() {
     directionNav:false
   });
 });
+
+
 
 // magnatic popup
 $(document).ready(function() {
@@ -83,3 +81,84 @@ $(document).ready(function() {
         fixedContentPos: false,
     });
 });
+
+
+
+// Clients Logos on Home page
+$('.clients-carousel').slick({
+  prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/arrow_left.png'>",
+  nextArrow:"<img class='a-right control-c next slick-next' src='../img/arrow_right.png'>",
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+
+//flexslider
+$('#what_clients_say').flexslider({
+	controlNav: false,
+	directionNav: false,
+	animation: "slide",
+	slideshowSpeed: 10000,
+	directionNav: true,
+	prevText: "Previous",
+	nextText: "Next",
+});
+
+
+
+$('.testimonials-area').slick({
+  prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/arrow_left.png'>",
+  nextArrow:"<img class='a-right control-c next slick-next' src='../img/arrow_right.png'>",
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+
+
+
+// Clients Served
+/*$(document).ready(function() {
+  $('#clients_served').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    //itemWidth: 210,
+    itemMargin: 3
+  });
+});*/
+
+
